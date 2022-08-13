99fc7dea21148c7bd6f0c5296f27cfcd2a0923ad
Pavan <pavanchambhare88@gmail.com>
2022-08-13 20:34:58 +0530

    first commit

---
+++ costomer.js
@@ -0,0 +1,5 @@
+ const person = {
+    name : "pavan"
+}
+
+export default person;
---
+++ script.js
@@ -0,0 +1,13 @@
+
+// import {hello} from './local.js';
+
+// hello()
+
+// let list = [ "audi" , "honda", "BMW"];
+// let  newLsst = ["farrari" , ...list];
+// console.log(newLsst)
+
+
+let person = {
+    name : "pavan"
+}
